e185920bcdd44684a38915b59ffc07c4a7c4638f
hafinisteam@gmail.com <hafinisteam@gmail.com>
2019-06-26 22:10:54 +0700

    Remove actions folder

--- src/state/ducks/actions/accessControl/types.js
+++
@@ -1,8 +0,0 @@
-export const LOAD_ACCESS_ROLE_LIST = 'accessControl/LOAD_ACCESS_ROLE_LIST';
-export const CREATE_ACCESS_ROLE = 'accessControl/CREATE_ACCESS_ROLE';
-export const EDIT_ACCESS_ROLE = 'accessControl/EDIT_ACCESS_ROLE';
-export const DELETE_ACCESS_ROLE = 'accessControl/DELETE_ACCESS_ROLE';
-
-export const LOAD_ROLE_PERMISSION_BY_ID = 'accessControl/LOAD_ROLE_PERMISSION_BY_ID';
-export const UPDATE_ROLE_PERMISSION = 'accessControl/UPDATE_ROLE_PERMISSION';
-export const DELETE_ROLE_PERMISSION = 'accessControl/UPDATE_ROLE_PERMISSION';
