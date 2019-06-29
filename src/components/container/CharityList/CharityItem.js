61051af8d9480ef127cc75fb9606ffd0d394065f
hafinisteam@gmail.com <hafinisteam@gmail.com>
2019-06-29 12:11:00 +0700

    Update config for production build

--- src/components/container/CharityList/CharityItem.js
+++ src/components/container/CharityList/CharityItem.js
@@ -1,64 +1,72 @@
-import React, { useState } from 'react';
-import { useTransition } from 'react-spring';
-import { UIButton } from '~/components/presentation/ui/button';
-import UICard from '~/components/presentation/ui/commons/UICard';
+import React, { useState } from "react";
+import { useTransition } from "react-spring";
+import { UIButton } from "~/components/presentation/ui/button";
+import UICard from "~/components/presentation/ui/commons/UICard";
 import OverlayUnit, {
-	OverlayWrapper
-} from '~/components/presentation/CharityList/OverlayUnit';
-import enhance from './withCharityItem';
+  OverlayWrapper
+} from "~/components/presentation/CharityList/OverlayUnit";
+import enhance from "./withCharityItem";
 
 const CharityItem = props => {
-	const { charity, handlePay, handleRadio, handleSelect, handleReset } = props;
-	const { amount, currency } = props;
+  const { charity, handlePay, handleRadio, handleSelect, handleReset } = props;
+  const { amount, currency } = props;
 
-	const [showOverlay, setOverlay] = useState(false);
+  const [showOverlay, setOverlay] = useState(false);
 
-	const transition = useTransition(showOverlay, null, {
-		from: { o: 0, scale: 1.1 },
-		enter: { o: 1, scale: 1 },
-		leave: { o: 0, scale: 1.1 }
-	});
+  const transition = useTransition(showOverlay, null, {
+    from: { o: 0, scale: 1.1 },
+    enter: { o: 1, scale: 1 },
+    leave: { o: 0, scale: 1.1 }
+  });
 
-	function closeOverlay() {
-		setOverlay(false);
-		handleReset();
+  function closeOverlay() {
+    setOverlay(false);
+    handleReset();
+  }
+
+  function payDonation() {
+    setOverlay(false);
+    handlePay();
 	}
 
-	function payDonation() {
-		setOverlay(false);
-		handlePay();
+	//dynamic loading image from resource
+	function loadImage(){
+		const images = require.context('../../../static/images', true);
+		return images('./' + props.charity.image);
 	}
 
-	return (
-		<UICard
-			image={`/images/${charity.image}`}
-			title={charity.name}
-			extraTitle={<UIButton onClick={() => setOverlay(true)}>Donate</UIButton>}
-			overlay={() =>
-				transition.map(
-					({ item, props: aniProps, key }) =>
-						item && (
-							<OverlayWrapper
-								key={key}
-								style={{
-									opacity: aniProps.o,
-									transform: aniProps.scale.interpolate(s => `scale(${s})`)
-								}}
-							>
-								<OverlayUnit
-									value={amount}
-									currency={currency}
-									onChangeRadio={handleRadio}
-									onSelect={handleSelect}
-									onCloseOverlay={closeOverlay}
-									onPayDonation={payDonation}
-								/>
-							</OverlayWrapper>
-						)
-				)
-			}
-		/>
-	);
+	const charityImg = loadImage();
+
+  return (
+    <UICard
+      image={charityImg}
+      title={charity.name}
+      extraTitle={<UIButton onClick={() => setOverlay(true)}>Donate</UIButton>}
+      overlay={() =>
+        transition.map(
+          ({ item, props: aniProps, key }) =>
+            item && (
+              <OverlayWrapper
+                key={key}
+                style={{
+                  opacity: aniProps.o,
+                  transform: aniProps.scale.interpolate(s => `scale(${s})`)
+                }}
+              >
+                <OverlayUnit
+                  value={amount}
+                  currency={currency}
+                  onChangeRadio={handleRadio}
+                  onSelect={handleSelect}
+                  onCloseOverlay={closeOverlay}
+                  onPayDonation={payDonation}
+                />
+              </OverlayWrapper>
+            )
+        )
+      }
+    />
+  );
 };
 
 export default enhance(CharityItem);
